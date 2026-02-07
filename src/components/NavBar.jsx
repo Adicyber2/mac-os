b6209ed09e16d68b84ee4aca52be390a2ee1785a
Adicyber2 <adityadeshmukh1404@gmail.com>
2026-02-07 13:46:17 +0530

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import DateTime from './DateTime'
+import "./nav.scss"
+const NavBar = () => {
+  return (
+    <nav>
+        <div className="left">
+            <div className="apple-icons">
+                <img src="/Navbar-icon/apple.svg" alt="" />
+            </div>
+
+            <div className="nav-item">
+                <p>Aditya Deshmukh</p>
+            </div>
+            <div className="nav-item">
+                <p>File</p>
+            </div>
+            <div className="nav-item">
+                <p>Window</p>
+            </div>
+            <div className="nav-item">
+                <p>Terminal</p>
+            </div>
+            
+
+        </div>
+
+
+        <div className="right">
+            <div className="nav-icon">
+                <img src="/Navbar-icon/wifi.svg" alt="" />
+            </div>
+
+            <div className="nav-item">
+                <DateTime/>
+            </div>
+
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
